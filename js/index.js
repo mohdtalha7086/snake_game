9520fea5276172155aaf4cc0ea84129f9ed6de29
Talha <mohdtalha7086@gmail.com>
2021-09-19 15:00:36 +0530

    fixed speed bug

--- js/index.js
+++ js/index.js
@@ -28,11 +28,13 @@ function isCollide(snake) {
     // If you bump into yourself 
     for (let i = 1; i < snakeArr.length; i++) {
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            speed = 5;
             return true;
         }
     }
     // If you bump into the wall
     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        speed = 5;
         return true;
     }
         
